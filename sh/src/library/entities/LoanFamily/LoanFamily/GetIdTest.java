64d532242d98b7ba6e1f0026a799dd4fbe35a476
shivani-sharma19 <shivani.sharma1915@gmail.com>
2016-09-27 10:20:24 +1000

    GetIDUnitTesting

---
+++ sh/src/library/entities/LoanFamily/LoanFamily/GetIdTest.java
@@ -0,0 +1,38 @@
+package library.entities.LoanFamily.LoanFamily;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import library.entities.BookFamily.BookFamily.Book;
+import library.entities.MemberFamily.MemberFamily.Member;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.IMember;
+
+public class GetIdTest {
+	Loan instance;
+	IBook book;
+	IMember  member;
+	Date borrowDate, returnDate;
+	@Before
+	public void setUp() throws Exception {
+		 book=new Book("Author1", "ITC515","0055",55);		
+			member=new Member ("James", "Mack" , "0456","jj",55);
+			borrowDate= new Date (2016,05,10);
+	        returnDate =new Date(2016,07,10);
+	        instance=new Loan(book, member, borrowDate, returnDate);
+	}
+
+
+	@Test
+	public final void testGetID() {
+int expectedResult=0;
+int result = instance.getID();
+assertEquals(expectedResult, result);
+	}
+
+}
