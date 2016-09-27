b433d77561935650ba1143c29dd454d2d4167f0e
shivani-sharma19 <shivani.sharma1915@gmail.com>
2016-09-27 10:23:19 +1000

    Loan Helper added

---
+++ sh/src/library/entities/LoanFamily/LoanFamily/LoanHelper.java
@@ -0,0 +1,19 @@
+package library.entities.LoanFamily.LoanFamily;
+
+import java.util.Date;
+
+import library.interfaces.daos.ILoanHelper;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
+public class LoanHelper implements ILoanHelper {
+
+	@Override
+	public ILoan makeLoan(IBook book, IMember borrower, Date borrowDate,
+			Date dueDate) {
+
+		return new Loan(book, borrower, borrowDate, dueDate);
+	}
+
+}
