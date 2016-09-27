299df04260f5d227bb88f8ec5c3e355c89d6c12e
shivani-sharma19 <shivani.sharma1915@gmail.com>
2016-09-27 12:40:16 +1000

    Testing

---
+++ sh/src/library/BorrowUC_CTLTest.java
@@ -0,0 +1,15 @@
+package library;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BorrowUC_CTLTest {
+
+	@Test
+	public void testCardSwiped()
+	{
+		fail("Not yet implemented");
+	}
+
+}
