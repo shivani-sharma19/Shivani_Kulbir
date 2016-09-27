ca1cff348b953ef6a6e99c1ec89c11985d497b74
shivani-sharma19 <shivani.sharma1915@gmail.com>
2016-09-27 13:15:05 +1000

    Integration Testing

---
+++ sh/src/library/entities/BookFamily/BookFamily/AllTests.java
@@ -0,0 +1,11 @@
+package library.entities.BookFamily.BookFamily;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GetAuthorTest.class, GetCallNumberTest.class, GetIdTest.class, GetTitleTest.class })
+public class AllTests {
+
+}
