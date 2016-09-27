fae1955865f24dc0cad22a3161437248cec1ef23
shivani-sharma19 <shivani.sharma1915@gmail.com>
2016-09-27 21:35:48 +1000

    Integration Testing Loan Family

---
+++ sh/src/library/entities/LoanFamily/LoanFamily/AllLoanFamilyTests.java
@@ -0,0 +1,11 @@
+package library.entities.LoanFamily.LoanFamily;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GetBookTest.class, GetBorrowerTest.class, GetIdTest.class, GetStateTest.class, IsOverdueTest.class })
+public class AllLoanFamilyTests {
+
+}
