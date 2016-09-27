91617818917dbcddd8db81770389dd3316df8653
shivani-sharma19 <shivani.sharma1915@gmail.com>
2016-09-27 13:06:24 +1000

    Testing

---
+++ sh/src/library/panels/borrow/SwipeCardPanelTest.java
@@ -0,0 +1,944 @@
+package library.panels.borrow;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SwipeCardPanelTest {
+
+	@Test
+	public void testDisplayErrorMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSwipeCardPanel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayMemberDetails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayExistingLoan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayOverDueMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayAtLoanLimitMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayOutstandingFineMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayOverFineLimitMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayScannedBookDetails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayPendingLoan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayConfirmingLoan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParamString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAccessibleContext() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUI() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUIClassID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJPanelLayoutManagerBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJPanelLayoutManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJPanelBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJPanel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUI() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUIPanelUI() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEnabled() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsDoubleBuffered() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVisible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetForeground() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBackground() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFont() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReshape() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBoundsRectangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSizeDimension() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLocationPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOpaque() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPreferredSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPreferredSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMinimumSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMinimumSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMaximumSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaximumSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAlignmentX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAlignmentY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBaseline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBaselineResizeBehavior() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRevalidate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGraphics() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFontMetrics() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRepaintLongIntIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetListeners() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessKeyEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessMouseEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessMouseMotionEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFocusTraversalKeys() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRequestFocus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRequestFocusBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRequestFocusInWindow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRequestFocusInWindowBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFirePropertyChangeStringBooleanBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFirePropertyChangeStringIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFirePropertyChangeStringCharChar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInsets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidateRoot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSafelyGetGraphicsComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSafelyGetGraphicsComponentComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGraphicsInvoked() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetManagingFocusForwardTraversalKeys() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetManagingFocusBackwardTraversalKeys() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetInheritsPopupMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInheritsPopupMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetComponentPopupMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComponentPopupMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUIComponentUI() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetComponentGraphics() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintChildren() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintBorder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintForceDoubleBuffered() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsPainting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintChildren() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintBorder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsPaintingTile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsPaintingForPrint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsManagingFocus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNextFocusableComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNextFocusableComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRequestFocusEnabled() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsRequestFocusEnabled() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGrabFocus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVerifyInputWhenFocusTarget() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVerifyInputWhenFocusTarget() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBorder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBorder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInsetsInsets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAlignmentY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAlignmentX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetInputVerifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInputVerifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDebugGraphicsOptions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDebugGraphicsOptions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShouldDebugGraphics() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterKeyboardActionActionListenerStringKeyStrokeInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComponentInputMapChanged() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterKeyboardActionActionListenerKeyStrokeInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnregisterKeyboardAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRegisteredKeyStrokes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConditionForKeyStroke() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActionForKeyStroke() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResetKeyboardActions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetInputMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInputMapInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInputMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetActionMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActionMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInputMapIntBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetActionMapBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRequestDefaultFocus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDefaultLocale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDefaultLocale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessComponentKeyEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessKeyBinding() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessKeyBindings() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessKeyBindingsForAllComponents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetToolTipText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetToolTipText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetToolTipTextMouseEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetToolTipLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPopupLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateToolTip() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testScrollRectToVisible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAutoscrolls() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAutoscrolls() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTransferHandler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTransferHandler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDropLocationForPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDropLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDndDone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSuperProcessMouseMotionEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCreatedDoubleBuffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCreatedDoubleBuffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClientProperty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutClientProperty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClientPropertyChanged() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUIProperty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsLightweightComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOpaque() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRectangleIsObscured() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeVisibleRectComponentRectangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeVisibleRectRectangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVisibleRect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFireVetoableChange() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddVetoableChangeListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveVetoableChangeListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVetoableChangeListeners() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTopLevelAncestor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAncestorListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveAncestorListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAncestorListeners() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRepaintRectangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOptimizedDrawingEnabled() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsPaintingOrigin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintImmediatelyIntIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintImmediatelyRectangle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAlwaysOnTop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPaintingChild() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test_paintImmediately() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPaintToOffscreen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckIfChildObscuredBySibling() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetWriteObjCounter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWriteObjCounter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDoubleBuffered() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRootPane() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompWriteObjectNotify() {
+		fail("Not yet implemented");
+	}
+
+}
