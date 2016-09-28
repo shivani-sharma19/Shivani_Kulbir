ea0b370159ed3754ff12682c95cb93b720c1aba5
kulbir <kulbirboparai97@gmail.com>
2016-09-29 00:06:28 +1000

    Test Successful-GetCallNumverTest.java

--- ITC515_NewLibrary/Unit_Testing/library/entities/GetCallNumberTest.java
+++ ITC515_NewLibrary/Unit_Testing/library/entities/GetCallNumberTest.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package library.entities;
 
 import static org.junit.Assert.*;
@@ -8,27 +11,46 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * @author sarvanjit
+ *
+ */
 public class GetCallNumberTest {
-
+Book instance;
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
+		instance =new Book("Author2","ITC515","565756",45);
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public final void test() {
-		fail("Not yet implemented"); // TODO
+		String expResult="565756";
+		String result=instance.getCallNumber();
+		assertEquals(expResult,result);
 	}
 
 }
