87a1e4b842cf256901ed92b582354f98d3f1065f
kulbir <kulbirboparai97@gmail.com>
2016-09-29 00:08:58 +1000

    Test Successful-GetIDTest.java

--- ITC515_NewLibrary/Unit_Testing/library/entities/GetIDTest.java
+++ ITC515_NewLibrary/Unit_Testing/library/entities/GetIDTest.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package library.entities;
 
 import static org.junit.Assert.*;
@@ -8,27 +11,32 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * @author sarvanjit
+ *
+ */
 public class GetIDTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
+    Book instance;
+	/**
+	 * @throws java.lang.Exception
+	 */
+		@Before
 	public void setUp() throws Exception {
+		instance =new Book("Author2","ITC515","565756",45);
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public final void test() {
-		fail("Not yet implemented"); // TODO
+		int expResult=45;
+		int result=instance.getID();
+		assertEquals(expResult,result);
 	}
 
 }
